fa4ddee070bbc021b305f638965cd0a4145fb4ca
Ygor <ygorespada14@gmail.com>
2023-08-11 10:44:47 -0300

    Edição de projeto

--- Aula 02 - 1108/NewDevProject/NewProjectDevSoftware/Program.cs
+++ Aula 02 - 1108/NewDevProject/NewProjectDevSoftware/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int auxiliar = 0;
+bool troca = false;
+int tamanho = 25;
+int[] vetor = new int[tamanho];
+Random random = new();
+
+Console.Clear();
+
+for (int i = 0; i < tamanho; i++)
+{
+    vetor[i] = random.Next(0, 500);
+}
+
+Array.Sort(vetor);
+
+ImprimirVetor();
+
+//Array.Sort(vetor);
+do{
+    troca = false;
+    for (int i = 0; i < tamanho - 1; i++)
+    {
+    if(vetor[i] > vetor[i + 1])
+        {
+        auxiliar = vetor[i];
+        vetor[i] = vetor[i + 1];
+        vetor[i + 1] = auxiliar;
+        troca = true;
+        }
+    }
+}while(troca);
+
+void ImprimirVetor()
+{
+    for (int i = 0; i < tamanho; i++)
+    {
+        Console.Write(vetor[i] + " ");
+    }
+    Console.WriteLine("\n");
+}
